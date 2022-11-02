fa36b4de7fb9b9fc3c28ba78ae6f3e133baeedb9
Di Lu <brotherludi@hotmail.com>
2022-11-01 22:05:53 -0600

    A function compare two values and tell us if they match or not.

---
+++ assertEqual.js
@@ -0,0 +1,21 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+//   } else {
+//     console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+//   }
+// };
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Hello", "Hello");
+assertEqual(1, 1);
+assertEqual(3, 5);
