90212be80fb5f35463aacacf071210df93225b7c
Di Lu <brotherludi@hotmail.com>
2022-11-08 21:48:51 -0700

    Takes in two arrays and returns true or false, based on a perfect match.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,8 @@
 //first compare 2 arrays
 const eqArrays = function (array1, array2) {
-  if (JSON.stringify(array1) === JSON.stringify(array2)) {
-    return true
-  } else {
-    return false
-  }
+    return JSON.stringify(array1) === JSON.stringify(array2)
 }
+
 //then compare 'array compare result' to expected 
 const assertEqual = function (eqArrays, expected) {
   if (eqArrays === expected) {
@@ -15,4 +12,9 @@ const assertEqual = function (eqArrays, expected) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
+
+// module.exports = {
+//   eqArrays:eqArrays,
+//   assertEqual:assertEqual,
+// }
