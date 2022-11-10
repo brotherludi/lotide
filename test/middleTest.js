3c67f4da0ae5d86452404d5354f68201b71f1242
Di Lu <brotherludi@hotmail.com>
2022-11-09 21:28:32 -0700

    Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -8,7 +8,6 @@
 // assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
 // assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
 
-
 const assert = require('chai').assert;
 const middle   = require('../middle');
 
